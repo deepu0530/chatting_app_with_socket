4479ab964dfd3a0feb842b69bd5a01dcef41575e
unknown <deepika@ldev.tech>
2021-12-15 14:06:45 +0530

    updated new design

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:sockets/utils/appcolors.dart';
 import 'package:web_socket_channel/io.dart';
 
 class ChattingScreen extends StatefulWidget {
@@ -47,99 +48,304 @@ class _ChattingScreenState extends State<ChattingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Crazy Chat"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 15,right: 15,bottom: 10),
+      backgroundColor: Colors.deepPurple[200],
+      body: Container(
+        margin: EdgeInsets.only(top: 35, bottom: 10),
+        decoration: BoxDecoration(
+            color: Colors.deepPurple[400],
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(50),
+                topLeft: Radius.circular(50),
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50))),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: ListView.separated(
-                padding: EdgeInsets.only(bottom: 10,top: 20),
-                itemCount: messages.length,
-                // itemCount: 4,
-                separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(
-                  height: 20,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
-                    mainAxisAlignment:
-                        messages[index]["username"] == widget.name
-                            ? MainAxisAlignment.end
-                            : MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 50, bottom: 25, left: 25, right: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
                     children: [
-                      Container(
-                        // width: MediaQuery.of(context).size.width*1.0,
-                        width: 250,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 15),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            borderRadius: BorderRadius.circular(20),
-                            color: messages[index]["username"] == widget.name
-                                ? Colors.purple[300]
-                                : Colors.purple[400],
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                               Text(
-                                messages[index]["username"],
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.grey[200]),
-                              ),
-                              SizedBox(height: 10,),
-                              Text(
-                                messages[index]["message"],
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white),
-                              )
-                            ],
-                          )),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Crazy Chat",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      )
                     ],
-                  );
-                },
+                  ),
+                  const Icon(
+                    Icons.message,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(right: 65, bottom: 0,top: 10),
-              padding: EdgeInsets.only(left: 15),
-
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35),
-                color: Colors.purple,
-              
-              ),
-              child: TextField(
-              
-                controller: _controller,
-                style: TextStyle(color: Colors.white),
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  labelText: "Type Message",
-                  labelStyle: TextStyle(color: Colors.grey[200],fontWeight: FontWeight.w400,fontSize: 16)
+            Expanded(
+              child: Container(
+                // margin: EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.separated(
+                        padding: EdgeInsets.only(
+                            bottom: 10, top: 20, right: 20, left: 20),
+                        itemCount: messages.length,
+                        // itemCount: 4,
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const SizedBox(
+                          height: 20,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Row(
+                            mainAxisAlignment:
+                                messages[index]["username"] == widget.name
+                                    ? MainAxisAlignment.end
+                                    : MainAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 30),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          messages[index]["username"],
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w700,
+                                              color: Colors.grey[400]),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          messages[index]["created"],
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w700,
+                                              color: Colors.grey[400]),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Container(
+                                      // width: MediaQuery.of(context).size.width*1.0,
+                                      width: 250,
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 30, vertical: 30),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.rectangle,
+                                        borderRadius: messages[index]
+                                                    ["username"] ==
+                                                widget.name
+                                            ? BorderRadius.only(
+                                                topLeft: Radius.circular(40),
+                                                topRight: Radius.circular(40),
+                                                bottomLeft: Radius.circular(40),
+                                                bottomRight: Radius.circular(2))
+                                            : BorderRadius.only(
+                                                topLeft: Radius.circular(40),
+                                                topRight: Radius.circular(40),
+                                                bottomRight:
+                                                    Radius.circular(40),
+                                                bottomLeft: Radius.circular(2)),
+                                        color: messages[index]["username"] ==
+                                                widget.name
+                                            ? Colors.deepPurple[100]
+                                            : Colors.deepPurple[200],
+                                      ),
+                                      child: Text(
+                                        messages[index]["message"],
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.black),
+                                      )),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    Container(
+                        width: double.infinity,
+                        height: 70,
+                        margin: const EdgeInsets.only(
+                            right: 20, bottom: 20, top: 10, left: 20),
+                        padding: EdgeInsets.only(left: 25,right: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(35),
+                          color: Colors.deepPurple[50]
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.share,size: 20,color: Colors.grey,),
+                            SizedBox(width: 20,),
+                            Expanded(child: Container(child: TextField(
+                              
+                              controller: _controller,
+                              cursorColor: AppColors.appcolor,
+                              decoration: InputDecoration(
+                                hintText: 'Type a Message.........',
+                                hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
+                                border: InputBorder.none
+                              ),
+                            ))),
+                            GestureDetector(
+        onTap: sendData,
+                              child: Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle, color: AppColors.appcolor),
+                                    child: Center(child: Icon(Icons.send,color: Colors.white,),),
+                              ),
+                            )
+                          ],
+                        )
+                        // TextField(
+                        //   controller: _controller,
+                        //   style: TextStyle(color: Colors.white),
+                        //   cursorColor: Colors.white,
+                        //   decoration: InputDecoration(
+                        //       border: InputBorder.none,
+                        //       labelText: "Type Message",
+                        //       labelStyle: TextStyle(
+                        //           color: Colors.grey[200],
+                        //           fontWeight: FontWeight.w400,
+                        //           fontSize: 16)),
+                        // )
+                        ),
+                  ],
                 ),
-              )
-            ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 0,top: 10),
-        child: FloatingActionButton(
-          
-          child: Icon(Icons.send),
-          onPressed: sendData,
-        ),
-      ),
+      
     );
+    // Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("Crazy Chat"),
+    //   ),
+    //   body: Padding(
+    //     padding: const EdgeInsets.only(left: 15,right: 15,bottom: 10),
+    //     child: Column(
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: <Widget>[
+    //         Expanded(
+    //           child: ListView.separated(
+    //             padding: EdgeInsets.only(bottom: 10,top: 20),
+    //             itemCount: messages.length,
+    //             // itemCount: 4,
+    //             separatorBuilder: (BuildContext context, int index) =>
+    //                 const SizedBox(
+    //               height: 20,
+    //             ),
+    //             itemBuilder: (BuildContext context, int index) {
+    //               return Row(
+    //                 mainAxisAlignment:
+    //                     messages[index]["username"] == widget.name
+    //                         ? MainAxisAlignment.end
+    //                         : MainAxisAlignment.start,
+    //                 children: [
+    //                   Container(
+    //                     // width: MediaQuery.of(context).size.width*1.0,
+    //                     width: 250,
+    //                       padding: EdgeInsets.symmetric(
+    //                           horizontal: 15, vertical: 15),
+    //                       decoration: BoxDecoration(
+    //                         shape: BoxShape.rectangle,
+    //                         borderRadius: BorderRadius.circular(20),
+    //                         color: messages[index]["username"] == widget.name
+    //                             ? Colors.purple[300]
+    //                             : Colors.purple[400],
+    //                       ),
+    //                       child: Column(
+    //                         crossAxisAlignment: CrossAxisAlignment.start,
+    //                         children: [
+    //                            Text(
+    //                             messages[index]["username"],
+    //                             style: TextStyle(
+    //                                 fontSize: 14,
+    //                                 fontWeight: FontWeight.w400,
+    //                                 color: Colors.grey[200]),
+    //                           ),
+    //                           SizedBox(height: 10,),
+    //                           Text(
+    //                             messages[index]["message"],
+    //                             style: TextStyle(
+    //                                 fontSize: 16,
+    //                                 fontWeight: FontWeight.w500,
+    //                                 color: Colors.white),
+    //                           )
+    //                         ],
+    //                       )),
+    //                 ],
+    //               );
+    //             },
+    //           ),
+    //         ),
+    // Container(
+    //   margin: const EdgeInsets.only(right: 65, bottom: 0,top: 10),
+    //   padding: EdgeInsets.only(left: 15),
+
+    //   decoration: BoxDecoration(
+    //     borderRadius: BorderRadius.circular(35),
+    //     color: Colors.purple,
+
+    //   ),
+    //   child: TextField(
+
+    //     controller: _controller,
+    //     style: TextStyle(color: Colors.white),
+    //     cursorColor: Colors.white,
+    //     decoration: InputDecoration(
+    //       border: InputBorder.none,
+    //       labelText: "Type Message",
+    //       labelStyle: TextStyle(color: Colors.grey[200],fontWeight: FontWeight.w400,fontSize: 16)
+    //     ),
+    //   )
+    // ),
+    //       ],
+    //     ),
+    //   ),
+    // floatingActionButton: Padding(
+    //   padding: const EdgeInsets.only(bottom: 0,top: 10),
+    //   child: FloatingActionButton(
+
+    //     child: Icon(Icons.send),
+    //     onPressed: sendData,
+    //   ),
+    // ),
+    // );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sockets/chat_screen.dart';
+import 'package:sockets/utils/appcolors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,93 +38,89 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.purple,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Set UserName & RoomName"),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: Text("Set UserName & RoomName"),
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20,right: 20,top: 50),
           child: Column(
             children: [
-              // Text("Set UserName & RoomName"),
+              // Icon(Icons.message,color: AppColors.appcolor,size: 120,),
               Container(
-                padding: const EdgeInsets.all(20.0),
-                margin: EdgeInsets.only(left: 30, right: 30, top: 10),
-                height: 300,
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
+                height: 250,
+                width: 350,
+                child: Image.asset("assets/images/chatimg.png",fit: BoxFit.cover,)),
+              SizedBox(height: 40,),
+              Text("Welcome Back",style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.w500),),
+              SizedBox(height: 40,),
+              Container(
+                  padding: EdgeInsets.only(left: 15),
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.purple),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Form(
-                      child: TextFormField(
-                        autofocus: false,
-                        cursorColor: Colors.white,
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                            labelText: "Enter User Name",
-                            labelStyle: TextStyle(color: Colors.grey[300]),
-                            enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                            
-                            ),
-                        controller: _usernamecontroller,
-                      ),
+                    color: Colors.deepPurple[50],
+                  ),
+                  child: TextField(
+                    controller: _usernamecontroller,
+                    
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w400),
+                    cursorColor: AppColors.appcolor,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "UserName",
+                        labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16)),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  padding: EdgeInsets.only(left: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.deepPurple[50],
+                  ),
+                  child: TextField(
+                    controller: _roomcontroller,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w400),
+                    cursorColor: AppColors.appcolor,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "RoomName",
+                        labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16)),
+                  )),
+              SizedBox(
+                height: 50,
+              ),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (Context) => ChattingScreen(
+                                  name: _usernamecontroller.text,
+                                  room: _roomcontroller.text,
+                                )));
+                },
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColors.appcolor,
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Form(
-                      child: TextFormField(
-                        cursorColor: Colors.white,
-                        autofocus: false,
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          labelText: "Enter Room Name",
-                          labelStyle: TextStyle(color: Colors.grey[300]),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                        ),
-                        controller: _roomcontroller,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    RaisedButton(
-                        color: Colors.white,
-                        child: Text(
-                          "Set",
-                          style: TextStyle(color: Colors.purple),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (Context) => ChattingScreen(
-                                        name: _usernamecontroller.text,
-                                        room: _roomcontroller.text,
-                                      )));
-                        })
-                  ],
+                    child: Center(child: Text("Submit",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 16),),),
                 ),
               ),
+              
             ],
           ),
         ),
