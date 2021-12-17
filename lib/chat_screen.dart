5f95bbd0f12af29cf7754fa8458370e5b297ff2e
unknown <deepika@ldev.tech>
2021-12-17 11:03:52 +0530

    updated

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -13,6 +14,7 @@ class ChattingScreen extends StatefulWidget {
 }
 
 class _ChattingScreenState extends State<ChattingScreen> {
+   ScrollController _scrollController = ScrollController();
   TextEditingController _controller = TextEditingController();
   var channel;
   List<Map<String, dynamic>> messages = [];
@@ -47,68 +49,73 @@ class _ChattingScreenState extends State<ChattingScreen> {
 
   @override
   Widget build(BuildContext context) {
+      Timer(
+    Duration(seconds: 1),
+    () => _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+  duration: Duration(milliseconds: 30),
+  curve: Curves.fastOutSlowIn,
+  ),
+  );
     return Scaffold(
-      backgroundColor: Colors.deepPurple[200],
-      body: Container(
-        margin: EdgeInsets.only(top: 35, bottom: 10),
-        decoration: BoxDecoration(
-            color: Colors.deepPurple[400],
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(50),
-                topLeft: Radius.circular(50),
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50))),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 50, bottom: 25, left: 25, right: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                          size: 25,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Crazy Chat",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      )
-                    ],
-                  ),
-                  const Icon(
-                    Icons.message,
-                    color: Colors.white,
-                    size: 25,
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                // margin: EdgeInsets.only(bottom: 10),
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.white),
-                child: Column(
+      backgroundColor: Colors.deepPurple[400],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 50, bottom: 25, left: 25, right: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
                   children: [
-                    Expanded(
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Crazy Chat",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+                const Icon(
+                  Icons.message,
+                  color: Colors.white,
+                  size: 25,
+                )
+              ],
+            ),
+          ),
+        
+          Expanded(
+            child: Container(
+              // margin: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  // borderRadius: BorderRadius.circular(50),
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(50),topLeft: Radius.circular(50)),
+                  color: Colors.white),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height -10,
+                    
                       child: ListView.separated(
+                        controller: _scrollController,
+                        shrinkWrap: true,
                         padding: EdgeInsets.only(
                             bottom: 10, top: 20, right: 20, left: 20),
                         itemCount: messages.length,
@@ -194,61 +201,61 @@ class _ChattingScreenState extends State<ChattingScreen> {
                         },
                       ),
                     ),
-                    Container(
-                        width: double.infinity,
-                        height: 70,
-                        margin: const EdgeInsets.only(
-                            right: 20, bottom: 20, top: 10, left: 20),
-                        padding: EdgeInsets.only(left: 25,right: 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(35),
-                          color: Colors.deepPurple[50]
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.share,size: 20,color: Colors.grey,),
-                            SizedBox(width: 20,),
-                            Expanded(child: Container(child: TextField(
-                              
-                              controller: _controller,
-                              cursorColor: AppColors.appcolor,
-                              decoration: InputDecoration(
-                                hintText: 'Type a Message.........',
-                                hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
-                                border: InputBorder.none
-                              ),
-                            ))),
-                            GestureDetector(
-        onTap: sendData,
-                              child: Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle, color: AppColors.appcolor),
-                                    child: Center(child: Icon(Icons.send,color: Colors.white,),),
-                              ),
-                            )
-                          ],
-                        )
-                        // TextField(
-                        //   controller: _controller,
-                        //   style: TextStyle(color: Colors.white),
-                        //   cursorColor: Colors.white,
-                        //   decoration: InputDecoration(
-                        //       border: InputBorder.none,
-                        //       labelText: "Type Message",
-                        //       labelStyle: TextStyle(
-                        //           color: Colors.grey[200],
-                        //           fontWeight: FontWeight.w400,
-                        //           fontSize: 16)),
-                        // )
-                        ),
-                  ],
-                ),
+                  ),
+                  Container(
+                      width: double.infinity,
+                      height: 70,
+                      margin: const EdgeInsets.only(
+                          right: 20, bottom: 20, top: 10, left: 20),
+                      padding: EdgeInsets.only(left: 25,right: 15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(35),
+                        color: Colors.deepPurple[50]
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.share,size: 20,color: Colors.grey,),
+                          SizedBox(width: 20,),
+                          Expanded(child: Container(child: TextField(
+                            
+                            controller: _controller,
+                            cursorColor: AppColors.appcolor,
+                            decoration: InputDecoration(
+                              hintText: 'Type a Message.........',
+                              hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
+                              border: InputBorder.none
+                            ),
+                          ))),
+                          GestureDetector(
+      onTap: sendData,
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle, color: AppColors.appcolor),
+                                  child: Center(child: Icon(Icons.send,color: Colors.white,),),
+                            ),
+                          ),
+                        ],
+                      )
+                      // TextField(
+                      //   controller: _controller,
+                      //   style: TextStyle(color: Colors.white),
+                      //   cursorColor: Colors.white,
+                      //   decoration: InputDecoration(
+                      //       border: InputBorder.none,
+                      //       labelText: "Type Message",
+                      //       labelStyle: TextStyle(
+                      //           color: Colors.grey[200],
+                      //           fontWeight: FontWeight.w400,
+                      //           fontSize: 16)),
+                      // )
+                      ),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
       
     );
